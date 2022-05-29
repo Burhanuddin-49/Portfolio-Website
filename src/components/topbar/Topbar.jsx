649e1458c19ecc2ad803965f5695773a74a85d3d
Burhanuddin-49 <burhanuddin9651@gmail.com>
2022-05-29 17:16:14 +0530

    Hamburger is added

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,9 +1,9 @@
 import { Mail, Person } from "@mui/icons-material";
 import "./topbar.scss";
 
-export default function Topbar() {
+export default function Topbar({ menuOpen, setMenuOpen }) {
   return (
-    <div className="topbar active">
+    <div className={"topbar " + (menuOpen && 'active')}>
       <div className="wrapper">
         <div className="left">
           <a href="#intro" className="logo">
@@ -19,7 +19,7 @@ export default function Topbar() {
           </div>
         </div>
         <div className="right">
-          <div className="hamburger">
+          <div className="hamburger" onClick={()=>setMenuOpen(!menuOpen)}>
             <span className="line1"></span>
             <span className="line2"></span>
             <span className="line3"></span>
