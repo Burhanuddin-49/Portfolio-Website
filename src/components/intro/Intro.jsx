5ffc77ec51ae890eb576261bcdc74ad099957748
Burhanuddin-49 <burhanuddin9651@gmail.com>
2022-05-31 18:26:43 +0530

    commit

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,6 +1,20 @@
+// import { init } from "ityped";
+// import { useEffect, useRef } from "react";
 import "./intro.scss";
 
 export default function Intro() {
+  // const textRef = useRef();
+
+  // useEffect(() => {
+  //   init(textRef.current, {
+  //     //This all ityped that doesn't working here
+  //     showCursor: false,
+  //     backDelay: 1500,
+  //     backSpeed: 60,
+  //     strings: ["Developer", "Designer", "Content Creator"],
+  //   });
+  // }, []);
+
   return (
     <div className="intro" id="intro">
       <div className="left">
