8ef48b58be8f61383f7a5ad5cdf152245ea8d64e
Burhanuddin-49 <burhanuddin9651@gmail.com>
2022-06-01 16:25:46 +0530

    Done portfolia List

--- src/components/portfolioList/PortfolioList.jsx
+++ src/components/portfolioList/PortfolioList.jsx
@@ -1,7 +1,12 @@
-import './portfolioList.scss'
+import "./portfolioList.scss";
 
-export default function PortfolioList() {
+export default function PortfolioList({ title, active, setSelected, id }) {
   return (
-    <div>PortfolioList</div>
-  )
+    <li
+      className={active ? "portfolioList active" : "portfolioList"}
+      onClick={() => setSelected(id)}
+    >
+      {title}
+    </li>
+  );
 }
