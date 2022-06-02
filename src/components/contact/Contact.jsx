fad73ad370dfe231f4c2abf087c96221070ef7b7
Burhanuddin-49 <burhanuddin9651@gmail.com>
2022-06-02 19:04:46 +0530

    Done with contact slide

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,9 +1,27 @@
+import { useState } from "react";
 import "./contact.scss";
 
 function Contact() {
+  const [message, setMessage] = useState(false);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setMessage(true);
+  };
+
   return (
     <div className="contact" id="contact">
-      Contact
+      <div className="left">
+        <img src="assets/shake.svg" alt="" />
+      </div>
+      <div className="right">
+        <h2>Contact</h2>
+        <form onSubmit={handleSubmit}>
+          <input type="email" placeholder="Email" />
+          <textarea placeholder="Message"></textarea>
+          <button type="submit">Submit</button>
+          {message && <span>Thanks, I'll will reply ASAP :)</span>}
+        </form>
+      </div>
     </div>
   );
 }
